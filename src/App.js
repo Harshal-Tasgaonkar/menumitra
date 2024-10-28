c0b82c788f07939dce8932a9beceadd38eaab900
unknown <harshaltasgaonkar06@gmail.com>
2024-10-28 20:30:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './Components/Home';
+
+import './Assets/Css/bootstrap.min.css'
+import './Assets/Css/style.css'
+import './Assets/Css/owl.theme.default.min.css'
+import './Assets/Css/responsive.css'
+import './Assets/Css/animate.min.css'
+import './Assets/Css/meanmenu.min.css'
+// import './Assets/Css/flaticon.css'
+import './Assets/Css/font-awesome.min.css'
+import './Assets/Css/owl.carousel.min.css'
+import './Assets/Css/nice-select.min.css'
+import './Assets/Css/odometer.min.css'
+import './Assets/Css/imagelightbox.min.css'
+import { HashRouter, Routes, Route } from "react-router-dom";
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Features from './Components/Features';
+import Pricing from './Components/Pricing';
+import Client from './Components/Client';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <>
+    <HashRouter>
+    <Routes>
+
+    <Route path="/" element={<Home />}/>
+    <Route path="/about" element={<About />}/>
+    <Route path="/contact" element={<Contact />}/>
+    <Route path="/features" element={<Features />}/>
+    <Route path="/pricing" element={<Pricing />}/>
+    <Route path="/client" element={<Client />}/>
+    
+
+    </Routes>
+    
+    </HashRouter>
+    </>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import "./Assets/js/contact-form-script.js"
+// import "./Assets/js/custom.js"
+// import "./Assets/js/jquery.min.js"
+// import './Assets/js/mixitup.min.js';
+// import './Assets/js/ajaxchimp.min.js';
+// import './Assets/js/appear.min.js';
+// import './Assets/js/bootstrap.bundle.min.js';
+// import './Assets/js/form-validator.min.js';
+// import './Assets/js/imagelightbox.min.js';
+// import './Assets/js/meanmenu.min.js';
+// import './Assets/js/nice-select.min.js';
+// import './Assets/js/odometer.min.js';
+// import './Assets/js/owl.carousel.min.js';
+// import './Assets/js/wow.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
